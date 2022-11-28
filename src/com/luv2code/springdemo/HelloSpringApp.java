0363018426ab5e236f42f594df7117549c3c2c99
KHOA121 <varus552@gmail.com>
2022-11-28 16:23:19 +0700

    Learning Spring

--- src/com/luv2code/springdemo/HelloSpringApp.java
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -6,7 +6,7 @@ public class HelloSpringApp {
     public static void main(String[] args) {
         System.out.println("Hello world");
         // Load the spring configuration file
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/luv2code/springdemo/applicationContext.xml");
         // Retrieve bean from spring container
         Coach theCoach = context.getBean("myCoach", Coach.class);
         System.out.println("Hello");
